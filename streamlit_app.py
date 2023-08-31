8bc2da401e4ab04bc68141e92d4f0f2981fca1e7
ertoalex <137882118+ertoalex@users.noreply.github.com>
2023-08-31 16:55:22 +0200

    Update streamlit_app.py
    added figures

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title(' My Parents Now Healthy Diner')
 
-streamlit.header('Breakfast Manu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Manu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
