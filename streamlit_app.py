df409b9abf9eda7ad0eec2de55429c121f211fd8
ertoalex <137882118+ertoalex@users.noreply.github.com>
2023-08-31 16:57:24 +0200

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 streamlit.title(' My Parents Now Healthy Diner')
 
-streamlit.header(' Breakfast Manu')
+streamlit.header(' Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
