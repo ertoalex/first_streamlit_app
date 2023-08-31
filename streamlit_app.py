b95f7663a28bdeb8018eac55bd978e05cde26196
ertoalex <137882118+ertoalex@users.noreply.github.com>
2023-08-31 17:22:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv ( "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Mettiamo un elenco di raccolta qui in modo che possano raccogliere i frutti che vogliono includere 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 my_fruit_list = my_fruit_list.set_index ( 'Fruit' ) # set_index permette di scegliere in base la colonna desiderata
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe ( my_fruit_list )  # visualizza l'elenco di raccolta
 
 
